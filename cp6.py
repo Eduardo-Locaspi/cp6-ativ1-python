3d35d2bc79c073a76bb9890fe45f34a45d248df9
Caio Tayra <caio.kenzo.t@gmail.com>
2025-10-21 21:56:04 -0300

    Finalizando as funções de listagem de registros

--- cp6.py
+++ cp6.py
@@ -11,76 +11,50 @@ def limpar_tela():
     os.system("cls" if os.name == "nt" else "clear")
 
 def menu():
-    limpar_tela()
-    print("Supermercado")
-    print()
-    print("0 - Sair")
-    print("1 - Cadastrar Produto")
-    print("2 - Pesquisar Produto")
-    print("3 - Listar todos os registros Cliente")
-    print("4 - Registrar regitro Produto")
-    print("5 - Apagar registro Produto ")
-    print()
- 
-    opcao()
+    while True:
+        limpar_tela()
+        print("Supermercado")
+        print()
+        print("0 - Sair")
+        print("1 - Cadastrar Produto")
+        print("2 - Pesquisar Produto")
+        print("3 - Listar registros Cliente")
+        print("4 - Editar regitros Produto")
+        print("5 - Apagar registros Produto ")
+        print()
 
-def pesquisa_string()->None:
-    limpar_tela()
-    valor_consultado = input("Qual o valor a ser consultado?_")
+        opcao = input("Escolha_")
+        match opcao:
+            case "0":
+                sair()
+                break
+            case "1":
+                cadastrar()
+            case "2":
+                pesquisar_produto()
+            case "3":
+                escolhaSubmenu()
+            case "4":
+                editar_produto()
+            case "5":
+                remover_produto()
+            case  _:
+                print("Opção selecionada não existe...")
 
-    print("----- PRODUTOS -----")
-    sql = "SELECT * FROM T_PRODUTO WHERE nm_produto LIKE ':1%'"
-    inst_consulta.execute(sql,(valor_consultado,))
+        input("Pressione ENTER para continuar...")  
 
-    
-    
-
- 
-def opcao():
-    opcao = input("Escolha_")
-    match opcao:
-        case "0":
-            global conexao
-            conexao = False
-        case "1":
-            cadastrar()
-        case "2":
-            pesquisar_produto()
-        case "3":
-            escolhaSubmenu()
-        case "4":
-            editar_produto()
-        case "5":
-            remover_produto()
-        case  _:
-            print("Opção selecionada não existe...")
-
-    input("Pressione ENTER para continuar...")  
-
-def escolhaSubmenu():
-    print("""a - Listar Todos
-          b - Pesquisar campo (String)
-          c - Pesquisar campo (numérico)""")
-    
-    opcao = input("Escolha:_").lower()
-    match opcao:
-        case "0":
-            ...
-        case "a":
-            listar_todos()
-        case "b":
-            pesquisa_string()
-        case _:
-            print("Valor inválido")
-
+def sair():
+    global conexao
+    print("Obrigado por utilizar o nosso código")
+    conexao = False
 
 def cadastrar():
     try:
         print("----- CADASTRAR PRODUTO -----")
         # Recebe os valores para cadastro
-        nome = input("{:.<25}:".format("Digite o nome do produto: "))
-        setor = input("{:.<25}:".format("Digite o setor: "))
-        data_str = input("{:.<25}:".format("Digite a data de vencimento (DD/MM/YYYY): "))
+        nome = input("{:.<25}:".format("Digite o nome do produto: ")).strip()
+        setor = input("{:.<25}:".format("Digite o setor: ")).strip()
+        data_str = input("{:.<25}:".format("Digite a data de vencimento (DD/MM/YYYY): ")).strip()
         preco = float(input("{:.<25}:".format("Digite o preço: ")))
         qt_produto = int(input("{:.<25}:".format("Digite a quatidade: ")))
 
@@ -88,6 +62,7 @@ def cadastrar():
         sql = """ INSERT INTO T_PRODUTO (nm_produto, setor_produto, dt_validade,preco_produto,qt_produto)VALUES (:1,:2,:3,:4,:5) """
         inst_cadastro.execute(sql,(nome, setor, validade, preco, qt_produto))
         conn.commit()
+        print("##### Produto cadastrado com sucesso! #####")
 
     except ValueError:
         print("Digite um valor válido!")
@@ -98,39 +73,46 @@ def cadastrar():
         print("##### Dados GRAVADOS #####")
 
 def pesquisar_produto():
-    limpar_tela()
-    print("----- PESQUISAR PRODUTO -----")
-    
-    sql = escolher_produto()
-    listar_dados(sql)
+    pesquisa = input("Produto: ")
+    sql = "SELECT * FROM T_PRODUTO WHERE nm_produto = :1"
+    listar_dados(sql, pesquisa)
 
-def escolher_produto():
-    pesquisa=input("Produto_ ")
-    return f"SELECT * FROM T_PRODUTO WHERE nm_produto='{pesquisa}'"
+def escolhaSubmenu():
+    print("""a - Listar Todos
+b - Pesquisar campo (String)
+c - Pesquisar campo (numérico)
+""")
+    
+    opcao = input("Escolha:_").lower()
+    match opcao:
+        case "0":
+            ...
+        case "a":
+            listar_todos()
+        case "b":
+            listar_string()
+        case _:
+            print("Valor inválido")
 
 def listar_todos():
     limpar_tela()
-    print("----- PRODUTOS -----")
     sql = "SELECT * FROM T_PRODUTO"
     listar_dados(sql)
 
 def listar_string():
     limpar_tela()
     valor_consultado = input("Qual o valor a ser consultado?_")
+    sql = "SELECT * FROM T_PRODUTO WHERE nm_produto LIKE :1"
+    parametro = f"%{valor_consultado}%"
+    listar_dados(sql, parametro)
 
-    print("----- PRODUTOS -----")
-    sql = "SELECT * FROM T_PRODUTO WHERE nm_produto LIKE '%:1'"
-    listar_dados(sql,valor_consultado)
-
-def listar_dados(sql: str, parametro:str =None) -> None:
+def listar_dados(sql: str, parametro:str = None) -> None:
     lista_produtos = []  # Lista para captura de dados do Banco
-
     if not parametro:
     # Instrução SQL com base no que foi selecinado na tela de menu
         inst_consulta.execute(sql)
     else:
         inst_consulta.execute(sql,(parametro,))
-
     # Captura todos os registros da tabela e armazena no objeto data
     data = inst_consulta.fetchall()
     # Insere os valores da tabela na Lista
@@ -145,19 +127,27 @@ def listar_dados(sql: str, parametro:str =None) -> None:
     if dados_df.empty:
         print(f"Não há produto cadastrado!")
     else:
+        print("----- PRODUTOS -----")
         print(dados_df)
 
 def editar_produto():
     try:
         id_prod = int(input("Qual id de produto iremos alterar? "))
-        # Escolher qual é o produto a ser alterado
-        # Escolher qual é o campo a ser alterado
-        campo_alterado= input("Qual é o campo à ser alterado? ")
-        #Qual o valor novo
-        valor_novo= input("Qual é o novo valor? ")
-        instrucao_sql = f"Update T_PRODUTO set {campo_alterado} = '{valor_novo}' WHERE cod_produto = {id_prod} "
-        #alterar o campo
-        inst_alteracao.execute(instrucao_sql)
+        campos_validos = ['nm_produto', 'setor_produto', 'dt_vencimento', 'preco_produto', 'peso']
+
+        campo_alterado = input(f"Qual campo deseja alterar ({', '.join(campos_validos)})? ").strip
+
+        if campo_alterado not in campos_validos:
+            print("Campo inválido!")
+            return
+        
+        valor_novo = input("Qual é o novo valor? ").strip()
+
+        instrucao_sql = f"UPDATE T_PRODUTO SET :1 = :2 WHERE cod_produto = :3"
+
+        inst_alteracao.execute(instrucao_sql, (campo_alterado, valor_novo, id_prod))
+        print("Produto atualizado com sucesso.")
+
     except ValueError:
         print("Digite um valor válido!")
     except:
@@ -167,7 +157,6 @@ def remover_produto():
     try:
         print("----- EXCLUIR PRODUTO -----")
         nm_prod = input("Digite o nome do produto que deseja remover:")
-
         sql = f"DELETE FROM T_PRODUTO WHERE nm_produto=:1"
         # Executa a instrução e atualiza a tabela
         inst_exclusao.execute(sql, (nm_prod,))
